ac05d1fbf961b660f40e7567bf2a6be677507546
Oosher <105375761+Oosher@users.noreply.github.com>
2023-11-07 17:39:13 +0200

    Update page.tsx

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -32,22 +32,11 @@ export default async function UserPage({params:{userId}}:Params) {
 
     const userData: Promise<User> = getUser(userId);
 
- 
-    
-
-
-
-
-
 /*     const [user,userPosts] = await Promise.all([userData,userPost]); */
 
 
  const user = await userData ;
     
-    
-    
-    
-  
   return <>
         <h2>{user.name}</h2>
         <Suspense fallback={<h2>Loading ....</h2>}> 
